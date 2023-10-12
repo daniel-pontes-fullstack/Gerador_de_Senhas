7839d2c214c26c5f1118358693ddae75bc3cce0d
daniel-pontes-fullstack <deslandev@gmail.com>
2023-10-11 22:56:30 -0300

    Pejeto gerador de senhas ver 1.0

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="pt_BR" data-theme="light" style="color-scheme: light;"> 
+  <head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="canonical" href="https://deslandev.com.br/">
+    <meta name="robots" content="index, follow">
+    <meta name="keywords" content="Deslan Dev, desenvolvedor web,seo,marketing digital,programação,cursos online,web design,front-end,web developer,back-end,php">
+    <meta name="description" content="Deslan Dev - Milhares de clientes satisfeitos!- Contamos com um universo de aplicativos em diversos setores no mercado de Programação Web e Web3, Games, Marketing Digital, Idiomas, Blockchain.">
+    <meta property="og:site_name" content="Deslan Dev">
+    <meta property="og:title" content="Grupo Deslan | Deslan Dev">
+    <meta property="og:description" content="Grupo Deslan - Milhares de clientes satisfeitos! - Contamos com um universo de aplicativos em diversos setores no mercado de Programação Web e Web3, Games, Marketing Digital, Idiomas, Blockchain.">
+    <meta property="og:url" content="https://deslandev.com.br/">
+    <meta property="og:image" content="https://deslandev.com.br/images/logo.svg">
+    <meta itemprop="name" content="Grupo Deslan | Deslan Dev">
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width">
+    <meta name="next-head-count" content="2">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6923391647659167"
+     crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@600&family=Inter:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/css/reset.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <title>Gerador de Senhas | Deslan</title>
+  </head>
+  <body>
+    <main>
+      <section class="hero">
+        <h1 class="title">Gerador de senha</h1>
+        <p class="subtitle">
+          Utilize o nosso gerador online para criar uma senha forte e segura.
+        </p>
+      </section>
+
+      <section class="box">
+        <div class="password">
+          <div class="text">
+            <input type="text" name="password" id="password" />
+          </div>
+          <div class="actions">
+            <button id="copy-1">
+              <img src="assets/images/copy.svg" width="42" />
+            </button>
+            <button id="renew">
+              <img src="assets/images/renew.svg" width="42" />
+            </button>
+          </div>
+        </div>
+        <div class="security-indicator">
+          <div id="security-indicator-bar" class="bar critical"></div>
+        </div>
+      </section>
+
+      <section class="box customize">
+        <h3 class="title">Personalizar</h3>
+        <div class="actions">
+          <div class="password-length">
+            <p>Tamanho: <span id="password-length-text">16</span></p>
+            <input
+              type="range"
+              name="password-length"
+              id="password-length"
+              class="slider"
+              value="16"
+              min="4"
+              max="64"
+            />
+          </div>
+          <div class="config">
+            <label class="checkbox-container">
+              <span class="text">Maiúsculas</span>
+              <input type="checkbox" id="uppercase-check" checked />
+              <span class="checkmark"></span>
+            </label>
+            <label class="checkbox-container">
+              <span class="text">Números</span>
+              <input type="checkbox" id="number-check" checked />
+              <span class="checkmark"></span>
+            </label>
+            <label class="checkbox-container">
+              <span class="text">Símbolos</span>
+              <input type="checkbox" id="symbol-check" checked />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+        </div>
+      </section>
+
+      <div class="submit">
+        <button id="copy-2">Copiar senha</button>
+      </div>
+      <div class="logo">
+        <p> Desenvolvido por:</p>
+        <a href="https://deslandev.com.br/"><img src="assets/images/logo1.svg"  alt=""></a>
+      </div>
+    </main>
+    <script src="assets/js/random.js"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6923391647659167"
+     crossorigin="anonymous"></script>
+<!-- password-generator -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6923391647659167"
+     data-ad-slot="1797851456"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</body>
+
+</html>
+
